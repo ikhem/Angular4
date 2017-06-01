1f6ef28487a0a49c1f500dd42c2afce021a7ea8b
Im Khem <im.khem@gmail.com>
2017-06-01 02:25:05 -0700

    Warning/Success Alert Components Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-warningalert',
+	template: '<h2>WARNING!!!</h2>',
+	styles: [`
+		h2 {
+			color: red;
+		}
+	`]
+})
+
+export class WarningAlertComponent{
+	
+}
